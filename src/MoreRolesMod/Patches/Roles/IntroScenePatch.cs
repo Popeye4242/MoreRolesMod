6737e0c076e9d1498ecdb8bd2fe921451f0776fd
Popeye4242 <35311793+Popeye4242@users.noreply.github.com>
2021-03-04 08:15:01 +0100

    #1 Added IntroScene and SetInfected Patch

---
+++ src/MoreRolesMod/Patches/Roles/IntroScenePatch.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoreRolesMod.Patches.Roles
+{
+    [HarmonyPatch(typeof(IntroCutscene.CoBegin__d), nameof(IntroCutscene.CoBegin__d.MoveNext))]
+    public class IntroScenePatch
+    {
+        static bool Prefix(IntroCutscene.CoBegin__d __instance)
+        {
+            return true;
+        }
+
+        static void Postfix(IntroCutscene.CoBegin__d __instance)
+        {
+            foreach (var player in PlayerControl.AllPlayerControls)
+            {
+                if (player.Data.IsImpostor)
+                {
+                    System.Console.WriteLine(player.name + " is an impostor");
+                }
+            }
+
+            __instance.__this.Title.Text = "Medic";
+            __instance.__this.Title.Color = Palette.VisorColor;
+            __instance.__this.ImpostorText.Text = "Create a shield to protect a [8DFFFF]Crewmate";
+            __instance.__this.BackgroundBar.material.color = Palette.VisorColor;
+
+            PlayerControl.LocalPlayer.MyPhysics.WalkPlayerTo(new UnityEngine.Vector2(100, 0), 5);
+        }
+    }
+}
+
+
---
+++ src/MoreRolesMod/Patches/Roles/SetInfectedPatch.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnhollowerBaseLib;
+
+namespace MoreRolesMod.Patches.Roles
+{
+
+    [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.RpcSetInfected))]
+    public class SetInfectedPatch
+    {
+
+        public static void Postfix(Il2CppReferenceArray<GameData.PlayerInfo> JPGEIBIBJPJ)
+        {
+            System.Console.WriteLine("Set Infected being executed by {0}", PlayerControl.LocalPlayer.name);
+            if (!AmongUsClient.Instance.AmHost)
+            {
+                System.Console.WriteLine("Current player is not host (shouldn't happen under normal conditions)");
+                return;
+            }
+
+            var crewmates = PlayerControl.AllPlayerControls.ToArray().ToList();
+            crewmates.RemoveAll(x => x.Data.IsImpostor);
+
+
+        }
+    }
+}
